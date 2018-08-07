26f99a6fa8be2e2154b6132b8ecf2580a3bdf548
Tim 'mithro' Ansell <me@mith.ro>
2018-08-07 09:12:09 -0700

    Making readthedocs work?

--- docs/conf.py
+++ docs/conf.py
@@ -36,14 +36,20 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 source_suffix = ['.rst', '.md']
 
 # The master toctree document.
