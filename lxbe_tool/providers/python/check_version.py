fd86a626b3544a281f787012637346be6110863c
Tim 'mithro' Ansell <me@mith.ro>
2018-08-07 21:16:12 -0700

    python: Adding some version checking.

--- lxbe_tool/providers/python/check_version.py
+++ lxbe_tool/providers/python/check_version.py
@@ -4,3 +4,37 @@
 "python --version"
 "Python 2.7.13"
 "Python 3.5.3"
+
+"""
+Just check you can import a module.
+
+function check_import {
+	MODULE=$1
+	if python3 -c "import $MODULE"; then
+		echo "$MODULE found"
+		return 0
+	else
+		echo "$MODULE *NOT* found!"
+		echo "Please try running the $SETUP_DIR/download-env.sh script again."
+		return 1
+	fi
+}
+"""
+
+"""
+Just check you can import a module and it is of the correct version.
+
+function check_import_version {
+	MODULE=$1
+	EXPECT_VERSION=$2
+	ACTUAL_VERSION=$(python3 -c "import $MODULE; print($MODULE.__version__)")
+	if echo "$ACTUAL_VERSION" | grep -q $EXPECT_VERSION > /dev/null; then
+		echo "$MODULE found at $ACTUAL_VERSION"
+		return 0
+	else
+		echo "$MODULE (version $EXPECT_VERSION) *NOT* found!"
+		echo "Please try running the $SETUP_DIR/download-env.sh script again."
+		return 1
+	fi
+}
+"""
